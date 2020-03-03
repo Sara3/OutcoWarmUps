8d6683beedbc909857aa2ba3a94b6955f7e66f67
sara3 <sadaqiq@gmail.com>
2020-03-02 16:52:21 -0800

    prompt

---
+++ winLoss.js
@@ -0,0 +1,9 @@
+// Win/Loss Rounds
+// Given a number of rounds in a game, return every permutation of wins and losses that may occur.
+// There will be no ties.
+//  The output should be an array of strings. A win will be represented with the letter X and a loss will be represented with the letter O.
+//  Example input 1: 3
+// output: ['XXX', 'XXO', 'XOX', 'OXX', 'XOO', 'OXO', 'OOX', 'OOO']
+
+// Example input 2: 2
+// output: []
