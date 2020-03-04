1d6cebc75cab6d1284a2470595d55661ec7ed7e1
sara3 <sadaqiq@gmail.com>
2020-03-03 16:51:54 -0800

    rainwaterPrompt

---
+++ trappingRainWater.js
@@ -0,0 +1,12 @@
+/*
+Given an array of integers representing the elevations of rocks, 
+determine the total amount of water that can be trapped between rocks.
+
+Input: 	 Array of integers
+Output: 	Integer
+
+Example
+
+Input: [3, 0, 2, 0, 4]      
+Output: 7
+*/
