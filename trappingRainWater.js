d274d2f70d99cda58109e311c66005443e724ac2
sara3 <sadaqiq@gmail.com>
2020-03-03 18:14:13 -0800

    solution

--- trappingRainWater.js
+++ trappingRainWater.js
@@ -10,3 +10,29 @@ Example
 Input: [3, 0, 2, 0, 4]      
 Output: 7
 */
+
+function trappingRainWater(arr){
+  let lMax = [0]
+  let rMax = []
+  let res = 0
+  // fill the lMax arr
+  for(let i =1; i < arr.length; i++){
+    let val = Math.max(arr[i-1], lMax[i - 1])
+    lMax.push(val)
+  }
+  //fill the rMax arr
+  rMax[arr.length-1] = 0
+  for(let j = arr.length -2 ; j >=0; j--){
+    rMax[j] = Math.max(arr[j+1], rMax[j+1])
+  }
+  
+  for(let i = 0; i < arr.length; i++){
+    let minWall = Math.min(lMax[i], rMax[i])
+    if(minWall - arr[i]>0){
+      res += minWall - arr[i]
+    }
+  }  
+  console.log(res)
+}
+
+trappingRainWater([3,0,2,0,4])
