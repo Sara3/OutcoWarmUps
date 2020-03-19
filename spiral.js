463add572ed3f7c9710181751be8f4942974d426
sara3 <sadaqiq@gmail.com>
2020-03-18 17:00:57 -0700

    Prompt

---
+++ spiral.js
@@ -0,0 +1,31 @@
+/*Matrices - N Spiral Matrix
+Prompt
+
+Given an integer N, output an N x N spiral matrix with integers 1 through N2.
+Examples:
+
+Input:  3
+
+Output: [[1, 2, 3],
+         [8, 9, 4],
+         [7, 6, 5]
+ 
+Input: 1
+
+Output: [[1]]
+
+Input:
+
+n = {Integer}
+Output
+
+result = {Integer[][]}
+Constraints:
+
+Time: O(N2)
+
+Space: O(N2)
+Resources:
+
+Spiral Matrix
+*/
