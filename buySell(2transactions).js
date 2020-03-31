cba310058efee529571281e1b248178491ff3b89
sara3 <sadaqiq@gmail.com>
2020-03-31 16:21:27 -0700

    explaination

--- buySell(2transactions).js
+++ buySell(2transactions).js
@@ -1,8 +1,16 @@
-/*Say you have an array for which the ith element is the price of a given stock on day i.
+
+
+/*
+
+
+
+
+ay you have an array for which the ith element is the price of a given stock on day i.
 
 Design an algorithm to find the maximum profit. You may complete at most two transactions.
 
-Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell 
+ the stock before you buy again).
 
 Example 1:
 
@@ -24,34 +32,77 @@ Example 3:
 Input: [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transaction is done, i.e. max profit = 0.
-
-
-https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-k-times/?ref=rp
-
 */
+
+
+
 
 
 
 function buySell(arr, k){
 
+  // Initialize a profit array that keeps track of the 
+  // max profit at any given point. We know the profit
+  // from when the input array is a single element large
+  // to when the array is completely accounted for
   let prof = []
+  
   console.log(arr)
+  
+  // Intialize the profit array to be all zeroes for now
   for(let i =0; i < arr.length; i++){
     prof.push(0)
   }
-
+  console.log("-------")
+  console.log(prof)
+  
   for(let j = 0; j < k; j++){
+    // We're going to perform the algorithm however many 
+    // k times. k is how many buy/sell operations we 
+    // can perform 
+    
+    // set up a min variable equal to the first element 
+    // of the array. As we find more minimum elements 
+    // later on, we'll instead select these as our min
+    // as it'll allow us to garner a greater difference
+    // between buy/sell prices
     let min = arr[0]
+    // max variable initialized to zero
     let max = 0
     for(let i = 1; i < arr.length; i++){
-      min = Math.min(min, arr[i] - prof[i]) 
-      /*Math.min compares two values and selects the more minimum value of the two.
-So before the Math.min statement -
-min has x value
-and arr[i] - prof[i] has y value.
-Math.min is comparing x and y. and it’s also setting min equal to the lesser value of x and y.
-*/
-      max = Math.max(max, arr[i]-min) 
+      // start the for loop from the second element
+      // we've already accounted for the first element 
+      // with lines 69 and 71
+      
+      // When we subtract the prof[i] from arr[i], we end up
+      // with the previous most minimum stock price we could've
+      // bought at
+      /* Small example: 
+      
+      input:                [0,3,1,5]
+      prof (1st iteration): [0,0,0,0]
+      min:                   0,0,0,0
+      max:                   0,3,1,5
+      
+      prof (2nd iteration): [0,3, 3, 5]
+      min:                   0,0,-2,-2
+      max:                   0,3, 3, 7
+      
+      So, where does this 7 at the end come from? 
+      Remember, it's a sum of two different buy/sell operations
+      We can see that it comes from the buy at 0, sell at 3 along
+      with the buy at 1, sell at 4. 
+      In code, it's calculated through 5 - (-2). So where did that
+      negative two come from? This was brought upon by us 
+      calculating a new min during the second external for loop
+      When we choose that min, we choose between (min, arr[i] - prof[i])
+      The only way we would ever get this negative min is when there's 
+      a drop from a higher stock price to a lower stock price following a 
+      point at which a sell should occur. 
+      This happens at index 2 of [0,3,1,5]
+      */
+      min = Math.min(min, arr[i] - prof[i]) //-2
+      max = Math.max(max, arr[i]-min) //3, 5-(-2)
       prof[i] = max
     }
     console.log(prof)
@@ -63,3 +114,5 @@ Math.min is comparing x and y. and it’s also setting min equal to the lesser v
 //[0, 3,3, 7]
 
 console.log(buySell([3, 3, 5, 0, 0, 3, 1, 4], 2))
+
+//6 
