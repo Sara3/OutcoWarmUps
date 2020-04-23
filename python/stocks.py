d24af537fbc640e546293794d99a3fa19aa7f617
Shohan Hasan <shohan.hasan@nyu.edu>
2020-04-23 17:04:24 -0400

    (feat) py implementation of nlogn stocks algorithm

--- python/stocks.py
+++ python/stocks.py
@@ -1,88 +1,84 @@
 from random import randint, seed
-from typing import Iterable, Iterator
 from heapq import nlargest
-from collections import deque, stack
+from collections import deque
+from itertools import starmap
 
 
-def _compact(it: Iterable[int]) -> Iterator[int]:
-    """ Sums up runs of same-sign ints """
-
-    def sign(x):
-        return -1 if x < 0 else 1
-
-    it = iter(it)
-    run = next(it, None)
-    if run is None:
-        return
-
-    for x in it:
-        if x == 0:
-            continue
-        if sign(run) == sign(x):
-            run += x
-        else:
-            yield run
-            run = x
-    yield run
-
-
-def _max_sums(it: Iterable[int], k: int = 1) -> int:
+def max_profits(price_intervals, k=1) -> int:
     """
-    Special purpose max subarray function.
-    Assuming input is alternating sign, we want to remove the negative numbers which
-    most augments the maximal sum.
+    We are given an input of (lo, hi) values representing an interval
+    where the price goes from a local minimum to a local maximum.
+    By comparing adjacent points, we can determine all possible profits
+    and return the k-largest ones.
+
+    Maintain a stack of intervals where the top represents our current profit window.
+    If the top of the stack is (lo1, hi1) and the next interval to consider is
+    (lo2, hi2), there are two special cases we need to handle:
+        a) lo2 < lo1: in this case, all gains from (lo1, hi1) are negated by the drop
+            from hi1 to lo2. Since we can't sell at the hi1 price after having bought
+            at lo2, we remove (lo1, hi1) off of the stack and place (lo2, hi2) there
+            as our new profit window. Record profit hi1 - lo1.
+        b) hi2 >= hi1 and low2 >= low1: in this case the larger single price window is
+            (lo1, hi2) but the larger total profit comes from the sum of both windows.
+            - Fortunately, we can store the difference between the hi1 and lo2, as a
+            future transaction: this is the cost we are paying to have this larger
+            single transaction over two separate transaction. If we have an extra
+            transaction to use later, we could offset this cost by having done two
+            transactions.
+    One final point: because we may have lots of local maximum profits inside larger
+    windows (handling case a may remove a hi value higher than the new hi, and case
+    b may take out a lo value lower than the new lo), we have to iteratively apply the
+    rules to make sure we account for all potential (lo, hi) values where lo comes
+    before hi.
     """
-    gains = deque(it)
-    if len(gains) == 0:
-        return [0]
+    profits = deque()
+    windows = deque()
 
-    # remove negatives from ends, if any
-    while len(gains) > 0 and gains[0] <= 0:
-        gains.popleft()
-    while len(gains) > 0 and gains[-1] <= 0:
-        gains.pop()
+    def case_a(lo2, hi2):
+        if len(windows) == 0:
+            return False
 
-    """
-    Starting with the first positive number, consider pairs of subsequent values
-    at a time. They will be of the form (-a, +b). If a+b > 0, we can add (a+b) to
-    our current sum and record |a| as a possible future improvement.
-    If a+b < 0, we have a local maximum but need to continue further to decide
-    whether it is part of a larger maximum. If the accumulated sum drops <= 0,
-    local values cannot contribute to a maximum down the road (Kadane's algorithm).
-    We can record the peak maximum as a candidate max and add all of the "-a"s
-    before local maxima as future improvements.
-    """
-    csum = peak = 0
-    local_improvements = stack()
-    local_peaks = stack()
-    best_transactions = deque()
-    for g in gains:
-        csum += g
-        if csum > peak:
-            # taller peak means we take any drops we have seen so far
-            # and add them as single transaction improvements
-            best_transactions.extend(local_improvements)
-            local_improvements.clear()
-            peak = csum
-        if csum <= 0:
-            best_transactions.append(peak)
-            best_transactions.extend(local_improvements)
-            local_improvements.clear()
-            csum = peak = 0
-            continue
+        lo1, hi1 = windows[-1]
+        if lo2 < lo1:
+            if hi1 - lo1 > 0:
+                profits.append(hi1 - lo1)
+            windows.pop()
+            return True
+        return False
 
-        # Note: this is skipped for any g dropping csum below 0 because they will
-        # never be inside a maximal subarray and therefore cannot be improvements.
-        if g < 0:
-            local_improvements.append(-g)
-        peak = max(peak, csum)
+    def case_b(lo2, hi2):
+        # This passes back the lower lo value as second return value
+        if len(windows) == 0:
+            return False, lo2
+        lo1, hi1 = windows[-1]
+        if hi2 >= hi1 and lo2 >= lo1:
+            # this is the price drop we can get rid of later using another
+            # transaction
+            p = hi1 - lo2
+            if p > 0:
+                profits.append(p)
+            windows.pop()
+            return True, lo1
+        return False, lo2
 
-    best_transactions.append(peak)
-    best_transactions.extend(local_improvements)
+    for lo2, hi2 in price_intervals:
+        while case_a(lo2, hi2):
+            pass
 
-    print(best_transactions)
-    # Use heapsort to get k-max
-    return nlargest(k, best_transactions)
+        _cont, lo2 = case_b(lo2, hi2)
+        while _cont:
+            _cont, lo2 = case_b(lo2, hi2)
+            pass
+
+        # Having resolved the above two cases, we add range (lo2, hi2) to stack
+        windows.append((lo2, hi2))
+
+    # pop out any remaining windows and add their profits
+    for lo, hi in windows:
+        profits.append(hi - lo)
+
+    # Use heapsort to return k-largest profits
+    return nlargest(k, profits)
 
 
 def stock_profits(prices, k=1):
@@ -98,18 +94,34 @@ def stock_profits(prices, k=1):
     For example:
         [1, 5, 9, 7, 2, 6] -diffs-> [4, 4, -2, -5, 4] -sums-> [8, -7, 4]
     """
-    diffs = (b - a for a, b in zip(prices, prices[1:]))
-    runs = _compact(diffs)
-    return _max_sums(runs, k)
+    n = len(prices)
+    if n < 2 or k < 1:
+        return 0
+    elif n == 2:
+        return max(0, prices[1]-prices[0])
+
+    intervals = deque()
+    lo, hi = prices[:2]
+    for x in prices[2:]:
+        while lo > hi:
+            lo, hi = hi, x
+            continue
+        while hi < x:
+            hi = x
+            continue
+
+        intervals.append((lo, hi))
+        lo, hi = hi, x
+
+    return max_profits(intervals, k)
 
 
 def main():
     days = 10
     k = 9
     # example = [1, 2, 3, 4, 10, 20, 0, 1, 20, 19, 4]
-    # seed(20200423)
-    # example = [randint(0, 20) for _ in range(days)]
-    example = [1, 7, 2, 4]
+    seed(20200423)
+    example = [randint(0, 20) for _ in range(days)]
     print("Example prices:\t", example)
     profits = stock_profits(example, k)
     print(f"Best {k} transactions:\t", profits)
